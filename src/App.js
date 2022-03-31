1c973550296be74006d052da565eb43fe5cb4921
samrecile <recilesam@gmail.com>
2022-03-31 12:55:26 -1000

    toggle border & delete tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appt',
+      day: 'Feb 5th at 2:30',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30',
+      reminder: true
+    }
+  ])
+
+
+  // delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ... task, reminder: !task.reminder } : task ))
+  }
+
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      ): (
+        'No tasks to show'
+        )}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <button className='btn'>Add</button>
+        <Button color='green' text='Add' onClikc={onClick}/>
     </header>
   )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker'
+    title: 'Task Tracker',
 }
 
 Header.propTypes = {
